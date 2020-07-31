4288a5d0fa922823a0509d457babc38aa48199ab
Dominik Maszczyk <Skitionek@gmail.com>
2020-08-01 01:39:19 +0200

    fix: wrong spread

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def generate_new_svg(stats: str) -> str:
 							html('rect', x=0, y=0, width=300, height=8, fill="white", rx=5)
 						),
 						START_COMMENT,
-						stats,
+						*stats,
 						END_COMMENT
 					)),
 				)
